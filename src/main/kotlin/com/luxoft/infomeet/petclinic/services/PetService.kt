2ea7e686161291ccc97eb1de1f6e4742cf15faf4
Koziolek <bjkuczynski@gmail.com>
2016-06-09 16:35:22 +0200

    singletony

--- src/main/kotlin/com/luxoft/infomeet/petclinic/services/PetService.kt
+++ src/main/kotlin/com/luxoft/infomeet/petclinic/services/PetService.kt
@@ -6,7 +6,7 @@ import com.luxoft.infomeet.petclinic.repository.PetRepository
 /**
  * Created by BKuczynski on 2016-06-08.
  */
-class PetService : GenericService<Pet, Long>(PetRepository()){
+object PetService : GenericService<Pet, Long>(PetRepository()){
 
     override fun byId(id: Long): Pet {
         println("Selecting pet with $id")
